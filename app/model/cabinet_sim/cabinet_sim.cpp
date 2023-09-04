18200c4a636e2f4df6018a9f3ace247c4e7ce639
Kamil Worek <kamil.worek@protonmail.com>
2023-09-05 00:19:05 +0200

    [WIP] Fast convolution tests

--- app/model/cabinet_sim/cabinet_sim.cpp
+++ app/model/cabinet_sim/cabinet_sim.cpp
@@ -21,22 +21,13 @@ using namespace mfx;
 cabinet_sim::cabinet_sim(const ir_t &ir) : effect { effect_id::cabinet_sim, "cabinet_sim" },
 ir{ir}
 {
-//    arm_fir_init_f32
-//    (
-//        &this->fir,
-//        this->fir_coeffs.size(),
-//        const_cast<float*>(this->fir_coeffs.data()),
-//        this->fir_state.data(),
-//        this->fir_block_size
-//    );
-
     this->input_buffer.fill(0);
 
     /* Precompute FFT of IR */
     arm_fill_f32(0.0f, this->ir_fft.data(), this->ir.size());
     arm_copy_f32(const_cast<float*>(this->ir.data()), this->ir_fft.data(), this->fft_size);
-    arm_cfft_radix4_init_f32(&this->fft, this->fft_size, 0, 1);
-    arm_cfft_radix4_f32(&this->fft, this->ir_fft.data());
+    arm_rfft_fast_init_f32(&this->fft, this->fft_size);
+    arm_rfft_fast_f32(&this->fft, this->ir_fft.data(), this->ir_fft.data() + this->fft_size, 0);
 }
 
 cabinet_sim::~cabinet_sim()
@@ -46,30 +37,28 @@ cabinet_sim::~cabinet_sim()
 
 void cabinet_sim::process(const dsp_input_t& in, dsp_output_t& out)
 {
-//    /* Apply speaker cabinet emulation using FIR filter */
-//    arm_fir_f32(&this->fir, const_cast<dsp_sample_t*>(in.data()), out.data(), out.size());
-
     const uint32_t block_size = in.size();
     const uint32_t move_size = this->input_buffer.size() - block_size;
 
-    /* Sliding window */
+    /* Sliding window of input blocks */
     arm_copy_f32(&this->input_buffer[block_size], &this->input_buffer[0], move_size);
     arm_copy_f32(const_cast<dsp_sample_t*>(in.data()), &this->input_buffer[move_size], block_size);
 
     /* FFT of sliding window */
     arm_fill_f32(0.0f, this->input_buffer_fft.data(), this->input_buffer_fft.size());
     arm_copy_f32(this->input_buffer.data(), this->input_buffer_fft.data(), this->input_buffer.size());
-    arm_cfft_radix4_init_f32(&this->fft, this->fft_size, 0, 1);
-    arm_cfft_radix4_f32(&this->fft, this->input_buffer_fft.data());
+    arm_rfft_fast_init_f32(&this->fft, this->fft_size);
+    arm_rfft_fast_f32(&this->fft, this->input_buffer_fft.data(), this->input_buffer_fft.data() + this->fft_size, 0);
 
     /* Multiplication (convolution) in frequency domain */
-    arm_cmplx_mult_cmplx_f32(this->ir_fft.data(), this->input_buffer_fft.data(), this->convolution.data(), this->fft_size);
+    arm_cmplx_mult_cmplx_f32(this->ir_fft.data() + this->fft_size, this->input_buffer_fft.data() + this->fft_size, this->convolution.data(), this->fft_size);
 
     /* Inverse FFT */
-    arm_cfft_radix4_init_f32(&this->fft, this->fft_size, 1, 1);
-    arm_cfft_radix4_f32(&this->fft, this->convolution.data());
+    arm_rfft_fast_init_f32(&this->fft, this->fft_size);
+    arm_rfft_fast_f32(&this->fft, this->convolution.data(), this->convolution.data() + this->fft_size, 1);
 
-    arm_copy_f32(&this->convolution[this->convolution.size() - block_size], out.data(), block_size);
+    /* Copy output */
+    arm_copy_f32(this->convolution.data() + this->convolution.size() - block_size, out.data(), block_size);
 }
 
 
