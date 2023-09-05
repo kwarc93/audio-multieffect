473e79bbe3a486517dd22a42acaa79dcec522741
Kamil Worek <kamil.worek@protonmail.com>
2023-09-05 13:09:21 +0200

    Add windowing to cabinet IR

--- scripts/cabinet_ir.m
+++ scripts/cabinet_ir.m
@@ -10,11 +10,17 @@ output_precision(8)
 % Script generates C++ array of samples from IR wave file
 
 % Load impulse response
-N = 2048;
 ir_file = '1960-G12M25-Starter/SM57/1960-G12M25-SM57-Cap45-0_5in.wav';
 [~,ir_name,~] = fileparts(ir_file);
 ir = wavread(ir_file);
+
+% Cut to desired length
+N = 1024;
 ir = ir(1:N);
+
+% Apply window after cut
+w = tukeywin(2*length(ir), 0.25);
+ir = ir.*w(length(w)/2+1:end);
 
 % Scale
 ir_pow = sum(ir.*ir)/length(ir);
@@ -27,21 +33,24 @@ filename = "impulse_response.c";
 fid = fopen(filename, "w");
 fprintf(fid, '/* Impulse response: %s */\n', ir_name);
 ir_name = ['ir_' strrep(ir_name, '-', '_')];
-fprintf(fid, 'std::array<%s, %d> %s\n{\n', type, N, ir_name);
+fprintf(fid, 'constexpr std::array<%s, %d> %s\n{\n', type, N, ir_name);
 
 i = 1;
-rows = 6;
-while N > 2
-  R = rows;
-  if N < rows R = N; end
-  N = N - R;
-  while R > 0
+cols = 6;
+rows = ceil(N / cols);
+while rows > 0
+  c = cols;
+  if N - i + 1 < c
+    c = rows;
+  end
+  rows = rows - 1;
+  while c > 0
     fprintf(fid, '    %.8f,', ir(i));
-    R = R - 1;
+    c = c - 1;
     i = i + 1;
   end
   fprintf(fid, '\n');
 end
-
+fseek(fid, ftell(fid)-1);
 fprintf(fid, '    %.8f\n};\n', ir(i));
 fclose(fid);
